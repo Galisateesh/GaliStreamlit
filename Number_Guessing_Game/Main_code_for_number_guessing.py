645b83933cba28a40a1f0f141b86e3d0d57c0cb2
Gali Sateesh <galisateesh542@gmail.com>
2026-04-09 20:30:41 +0530

    Update Main_code_for_number_guessing.py

--- Number_Guessing_Game/Main_code_for_number_guessing.py
+++ Number_Guessing_Game/Main_code_for_number_guessing.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-st.markdown("<h4>{st.session_state.a}</h4>", unsafe_allow_html=True)
+st.markdown(f"<h4>{st.session_state.a}</h4>", unsafe_allow_html=True)
 st.title("🎯Deeksha Guess the Number 🎮 !!! 🔢")
 file_path="Number_Guessing_Game/Number.css"
 with open(file_path) as p:
